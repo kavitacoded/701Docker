2a28e4681c13d4e038ccde8445a48c5992913e1a
kavitacoded <kavi20900@gmail.com>
2024-07-22 12:40:46 +0530

    docker spring boot rest

---
+++ src/main/java/com/nt/rest/ActorOperationController.java
@@ -0,0 +1,20 @@
+package com.nt.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/actor")
+public class ActorOperationController {
+
+	@GetMapping("/wish/{name}")
+	public ResponseEntity<String> showActor(@PathVariable String name){
+		return new ResponseEntity<String>(HttpStatus.OK);
+	}
+	
+	
+}
